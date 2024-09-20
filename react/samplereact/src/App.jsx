835184b5e9357a1ed42972a4626cde997e895ad9
useru1k <uvvi571@gmail.com>
2024-09-20 22:55:59 +0530

    Creating react router dom

--- react/samplereact/src/App.jsx
+++ react/samplereact/src/App.jsx
@@ -2,14 +2,25 @@ import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import About from "./components/About";
 import ContactSection from "./components/ContactSection";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import {BrowserRouter ,Routes, Route} from "react-router-dom";
 
 function App(){
   return (
   <>  
-    <Navbar />
+    {/* <Navbar /> */}
     {/* <Profile /> */}
     {/* <About /> */}
-    <ContactSection />
+    {/* <ContactSection /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/ContactPage" element={<ContactPage />}></Route>
+        <Route path="/AboutPage" element={<AboutPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   </>
   )
 }
