b39bc62ef5924f37f27dcb2e9e4cc10ca7b035a2
useru1k <uvvi571@gmail.com>
2024-09-23 13:53:02 +0530

    update phase2

---
+++ stack/server/server.js
@@ -0,0 +1,18 @@
+console.log("Hello word");
+
+// const express = require('express')
+// const  app = express()
+
+// require('dotenv').config()
+
+// const port = process.env.PORT || 7778
+
+// app.use(express.json())
+
+// app.get ('/',(req,res)=>{
+//     res.json({message:"Welcome"})
+// })
+
+// app.listen(port, ()=>{
+//     console.log("Wel")
+// })
